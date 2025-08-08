8688458d80edfc4b91151984b0eaf408412d8a89
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-08 08:10:37 -0300

    chore: first commit

---
+++ src/main/java/io/github/Guimaraes131/super_poderes_inuteis/SuperPoderesInuteisApplication.java
@@ -0,0 +1,13 @@
+package io.github.Guimaraes131.super_poderes_inuteis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperPoderesInuteisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperPoderesInuteisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/Guimaraes131/super_poderes_inuteis/SuperPoderesInuteisApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Guimaraes131.super_poderes_inuteis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperPoderesInuteisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
